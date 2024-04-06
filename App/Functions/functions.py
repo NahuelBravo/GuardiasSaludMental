c5c975920f3d7fb18811f4c0450046b35b2237a8
Nahuel Bravo <87450107+NahuelBravo@users.noreply.github.com>
2024-04-06 20:43:35 -0300

    ajustes de modelos

--- App/Functions/functions.py
+++ App/Functions/functions.py
@@ -1,16 +1,2 @@
 import calendar
 import datetime
-
-# Especifica el año y el mes del que quieres obtener las fechas
-año = 2022
-mes = 4  # Abril
-
-# Obtén el último día del mes
-ultimo_dia_mes = calendar.monthrange(año, mes)[1]
-
-# Genera todas las fechas del mes
-fechas_mes = [datetime.date(año, mes, dia) for dia in range(1, ultimo_dia_mes + 1)]
-
-# Imprime todas las fechas del mes
-for fecha in fechas_mes:
-    print(fecha)
